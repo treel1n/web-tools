fbc14af8d2b2a9420b3cd4934b74db9c5bd037b4
linshu <lin@oray.com>
2023-03-13 13:16:40 +0800

    feat: add service worker register

--- src/utils/swRegister.ts
+++ src/utils/swRegister.ts
@@ -1,14 +1,14 @@
 export default function() {
-  // if ('serviceWorker' in navigator) {
-  //   // Wait for the 'load' event to not block other work
-  //   window.addEventListener('load', async () => {
-  //     // Try to register the service worker.
-  //     try {
-  //       const reg = await navigator.serviceWorker.register('./service-worker.js', { scope: './' });
-  //       console.info('Service worker registered! 😎', reg);
-  //     } catch (err) {
-  //       console.error('😥 Service worker registration failed: ', err);
-  //     }
-  //   });
-  // }
+  if ('serviceWorker' in navigator) {
+    // Wait for the 'load' event to not block other work
+    window.addEventListener('load', async () => {
+      // Try to register the service worker.
+      try {
+        const reg = await navigator.serviceWorker.register('./service-worker.js', { scope: './' });
+        console.info('Service worker registered! 😎', reg);
+      } catch (err) {
+        console.error('😥 Service worker registration failed: ', err);
+      }
+    });
+  }
 }
