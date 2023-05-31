e36ce2dd61370ef83aecc68d7345653813c6cee4
treel1n <treel1n@users.noreply.github.com>
2023-05-31 03:18:13 +0000

    deploy: 2910efa6a5fc1cf2c8c1ff0d677cafc38b3448e6

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/web-tools/sw.js', { scope: '/web-tools/' })})}
